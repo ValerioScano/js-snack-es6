2b3b4d6f8b0894868191da427178b893a92e2487
ValerioScano <valerioscano00@gmail.com>
2025-11-15 16:22:49 +0100

    first exercise

---
+++ main.js
@@ -0,0 +1,13 @@
+const biciclette = [{nome:"wilier", peso : 60}, {nome: "megamo", peso: 30}, {nome: "megamoPulse", peso: 40}, {nome: "berria", peso:20}]
+
+document.getElementById("inizia").addEventListener("click", function(event) {
+    event.preventDefault();
+    debugger
+    let biciclettaLeggera = biciclette[0]
+    for (i=0; i < biciclette.length; i++) {
+        if (biciclette[i].peso < biciclettaLeggera) {
+            biciclettaLeggera = biciclette[i]
+        }
+    }
+    document.getElementById("contenitore").innerText = `la bicicletta più leggera è la ${biciclettaLeggera.nome} che pesa ${biciclettaLeggera.peso} kg!`
+})
